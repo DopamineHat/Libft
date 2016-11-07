696e8ee0e672f2de76dbadd2f6220799231b9a97
Romain PAGOT <rpagot@e3r3p17.42.fr>
2016-11-07 04:25:22 +0100

    strcat

--- srcs/ft_strcat.c
+++ srcs/ft_strcat.c
@@ -1,21 +1,22 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   strncpy.c                                          :+:      :+:    :+:   */
+/*   strcat.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: rpagot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2016/11/07 04:02:34 by rpagot            #+#    #+#             */
-/*   Updated: 2016/11/07 04:06:51 by rpagot           ###   ########.fr       */
+/*   Created: 2016/11/07 04:17:00 by rpagot            #+#    #+#             */
+/*   Updated: 2016/11/07 04:24:33 by rpagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char	*ft_strncpy(char *dst, const char *src, size_t len)
+char	*ft_strcat(char *s1, char *s2)
 {
-	while (*src && --len >= 0)
-		*dst++ = *src++;
-	*dst = *src;
-	return (dst);
+	while (*s1++)
+		;
+	while (*s1)
+		*s1++ = *s2;
+	return (s1);
 }
