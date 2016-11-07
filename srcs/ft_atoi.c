f8ff4de943acb31733927727925b175c412d388f
Romain PAGOT <rpagot@e3r3p17.42.fr>
2016-11-07 02:20:23 +0100

    atoi

--- srcs/ft_atoi.c
+++ srcs/ft_atoi.c
@@ -6,36 +6,32 @@
 /*   By: adeletan <adeletan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/06 01:51:33 by adeletan          #+#    #+#             */
-/*   Updated: 2016/11/07 02:13:48 by rpagot           ###   ########.fr       */
+/*   Updated: 2016/11/07 02:18:50 by rpagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include "libft.h"
-
-int		ft_atoi(char const *str)
+int ft_atoi(char *str)
 {
-	cacaint nb;
-	int index;
+	int i;
+	int s;
+	int x;
 
-	nb = 0;
-	index = 0;
-	if (strcmp(str, "-2147483648") == 0)
-		return (-2147483648);
-	if ((str[index] == '-' || str[index] == '+') ||
-		(str[index] >= '0' && str[index] <= '9'))
+	i = 0;
+	s = 1;
+	x = 0;
+	if (str[i] == ' ' || str[i] == '\t' || str[i] == '\v' || str[i] == '\f'
+		|| str[i] == '\n' || str[i] == '\r' || str[i] == '+')
+		++i;
+	if (str[i] == '-')
 	{
-		if (str[index] == '-'
-			&& str[index + 1] >= '0' && str[index + 1] <= '9')
-		{
-			++index;
-		}
-		else if (str[index] == '+' &&
-				str[index + 1] >= '0' && str[index + 1] <= '9')
-			++index;
-		while (str[index] >= '0' && str[index] <= '9')
-			nb = (nb * 10) + str[index++] - '0';
+		s = -1;
+		++i;
 	}
-	if (str[0] == '-')
-		nb = -nb;
-	return (nb);
+	while (str[i] <= '9' && str[i] >= '0')
+	{
+		x *= 10;
+		x += (str[i] - '0');
+		++i;
+	}
+	return (x * s);
 }
