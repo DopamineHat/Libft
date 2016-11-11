e7f0a356e1c541765218a0df584f71969c2bc37c
Romain PAGOT <rpagot@e3r6p16.42.fr>
2016-11-11 17:46:13 +0100

    fix

--- srcs/ft_strmapi.c
+++ srcs/ft_strmapi.c
@@ -6,7 +6,7 @@
 /*   By: adeletan <adeletan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 07:25:11 by adeletan          #+#    #+#             */
-/*   Updated: 2016/11/06 04:33:46 by adeletan         ###   ########.fr       */
+/*   Updated: 2016/11/11 17:44:50 by rpagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ char	*ft_strmapi(char const *s, char (*f) (unsigned int, char))
 	index = 0;
 	while (s[index])
 		++index;
-	if (!(array = (char*)malloc(index * sizeof(char))))
+	if (!(array = (char*)malloc((index + 1) * sizeof(char))))
 		return (NULL);
+	array[index] = '\0';
 	index = 0;
 	while (s[index])
 	{
 		array[index] = f(index, s[index]);
 		++index;
 	}
-	array[index] = '\0';
 	return (array);
 }
