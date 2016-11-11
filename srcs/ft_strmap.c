d79f5d57ee96467a4cbe1930396b8424623d5b9b
Romain PAGOT <rpagot@e3r6p16.42.fr>
2016-11-11 17:42:21 +0100

    strmap

--- srcs/ft_strmap.c
+++ srcs/ft_strmap.c
@@ -6,7 +6,7 @@
 /*   By: adeletan <adeletan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/05 07:35:55 by adeletan          #+#    #+#             */
-/*   Updated: 2016/11/06 05:50:22 by adeletan         ###   ########.fr       */
+/*   Updated: 2016/11/11 17:40:45 by rpagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ char	*ft_strmap(char const *s, char (*f) (char))
 	index = 0;
 	while (s[index])
 		++index;
-	if (!(array = (char*)malloc(index * sizeof(char))))
+	if (!(array = (char*)malloc((index + 1) * sizeof(char))))
 		return (NULL);
+	array[index] = '\0';
 	index = 0;
 	while (s[index])
 	{
 		array[index] = f(s[index]);
 		++index;
 	}
-	array[index] = '\0';
 	return (array);
 }
