e020826e21a2ac6fe2f1cb9137855254c811fb64
Romain PAGOT <rpagot@e3r6p16.42.fr>
2016-11-12 21:23:17 +0100

    hello

--- srcs/ft_strnstr.c
+++ srcs/ft_strnstr.c
@@ -6,7 +6,7 @@
 /*   By: adeletan <adeletan@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 05:23:06 by adeletan          #+#    #+#             */
-/*   Updated: 2016/11/07 06:31:52 by adeletan         ###   ########.fr       */
+/*   Updated: 2016/11/12 21:17:26 by rpagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,25 +14,25 @@
 
 char	*ft_strnstr(char const *big, char const *little, size_t len)
 {
-	int		index;
-	int		index2;
-	size_t	poubelle;
+	int		i;
+	int		i2;
+	size_t	trash;
 
 	if (len > (unsigned long)ft_strlen(little))
-		poubelle = (unsigned long)ft_strlen(little);
-	index = 0;
-	index2 = -1;
-	if (!(little[index]))
+		trash = (unsigned long)ft_strlen(little);
+	i = 0;
+	i2 = -1;
+	if (!(little[i]))
 		return ((char*)big);
-	while (big[index] && (index + ft_strlen(little) <= (int)len))
+	while (big[i] && (i + ft_strlen(little) <= (int)len))
 	{
-		if (big[index] == little[0])
-			if (!(ft_strcmp(ft_strsub(big, index, poubelle),
-						ft_strsub(little, 0, poubelle))) && index2 == -1)
-				index2 = index;
-		++index;
+		if (big[i] == little[0])
+			if (!(ft_strcmp(ft_strsub(big, i, trash),
+						ft_strsub(little, 0, trash))) && i2 == -1)
+				i2 = i;
+		++i;
 	}
-	if (index2 == -1)
+	if (i2 == -1)
 		return (NULL);
-	return ((char*)(&big[index2]));
+	return ((char*)(&big[i2]));
 }
