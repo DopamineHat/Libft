1cca8bf2fcb8e9f4e22a7a02ee256646e4bffe41
Romain PAGOT <rpagot@e3r8p10.42.fr>
2016-11-13 22:19:03 +0100

    noerrors

--- srcs/ft_strncat.c
+++ srcs/ft_strncat.c
@@ -6,7 +6,7 @@
 /*   By: rpagot <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/11/07 05:29:50 by rpagot            #+#    #+#             */
-/*   Updated: 2016/11/11 20:08:13 by rpagot           ###   ########.fr       */
+/*   Updated: 2016/11/13 22:00:27 by rpagot           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,14 +23,13 @@ char	*ft_strncat(char *s1, char const *s2, size_t n)
 	limit = ft_strlen((char*)s2);
 	while (s1[i])
 		++i;
-	while (i2 < n)
+	while (i2 < n && s2[i2])
 	{
 		if (i2 < limit)
 			s1[i] = s2[i2];
-		else
-			s1[i] = '\0';
 		++i2;
 		++i;
 	}
+	s1[i] = '\0';
 	return (s1);
 }
